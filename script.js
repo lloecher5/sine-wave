07645cb6d832543adc7ef82cffb25e8f1c6b9390
Luke Loecher <Luke.Loecher@ibm.com>
2022-07-27 12:41:45 -0400

    added comments to describe code

--- script.js
+++ script.js
@@ -18,17 +18,24 @@ lengthDisplay.innerHTML = wave.length;
 amplitudeDisplay.innerHTML = wave.amplitude;
 frequencyDisplay.innerHTML = wave.frequency;
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = innerWidth * 0.5;
+canvas.height = innerHeight * 0.5;
 
 let increment = wave.frequency;
 
+//function used to create the animated sine wave
 function animate() {
+  //tells the browser that you wish to perform an animation and requests that the browser calls a specified function to update an
+  //animation before the next repaint
   window.requestAnimationFrame(animate);
+  //draws a filled rectangle
   c.fillRect;
+  //clears a recatangle within a given rectangle
   c.clearRect(0, 0, canvas.width, canvas.height);
 
+  //begins a path
   c.beginPath();
+  //moves the path to a location
   c.moveTo(0, canvas.height / 2);
   //this for loop wil run for each pixel along the canvas's width
   for (let i = 0; i < canvas.width; i++) {
@@ -38,13 +45,14 @@ function animate() {
     );
   }
   c.strokeStyle = "hsl(200, 50%, 60%)";
+  //actually draws the path with all the moveTo and lineTo methods.
   c.stroke();
   increment += wave.frequency;
 }
 
 animate();
 
-//functions used to update the values in the HTML
+//functions used to update the wave values
 function updateAmplitude() {
   var amplitude = document.querySelector("#amplitude").value;
   amplitudeDisplay.innerHTML = amplitude;
